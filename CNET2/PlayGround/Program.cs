865cf7641109a4dc4a243438c02ef1ec3cb25fbe
v5416080 <vaclav.kure@saint-gobain.com>
2021-11-22 10:54:50 +0100

    Metoda na tisk listu do konzole

--- CNET2/PlayGround/Program.cs
+++ CNET2/PlayGround/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
-//
+var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+
+var vysledek = strings.Select( x => x.ToUpper());
+//Debug.WriteLine(vysledek);
+
+PrintList(vysledek.ToList());
+
+static void PrintList(List<string> coVypsat)
+{
+
+    foreach (var item in coVypsat)
+    {
+        Console.WriteLine(item);
+    }
+}
