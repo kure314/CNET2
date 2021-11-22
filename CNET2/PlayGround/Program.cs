de2ec3874eb5f066ddff1a988b412a180053e7e3
v5416080 <vaclav.kure@saint-gobain.com>
2021-11-22 14:23:43 +0100

    Slovník  definice

--- CNET2/PlayGround/Program.cs
+++ CNET2/PlayGround/Program.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 using PlayGround;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
@@ -10,23 +10,23 @@ var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "sev
 var vysledek = strings.Select( x => x.ToUpper());
 //Debug.WriteLine(vysledek);
 
-PrintList(vysledek.ToList());
+//PrintList(vysledek.ToList());
 bool isOnlyEvenNumber = numbers.All(x => x % 2 == 0);
-Console.WriteLine($"Jsou všechna čísla sudá? {isOnlyEvenNumber}");
+//Console.WriteLine($"Jsou všechna čísla sudá? {isOnlyEvenNumber}");
 
 var slova = numbers.Select(x => strings[x]);
 
-PrintList(slova.ToList()); 
+//PrintList(slova.ToList()); 
 
 foreach (var item in numbers)
 {
-    Console.WriteLine(strings[item]);
+   // Console.WriteLine(strings[item]);
 }
 
 /// <summary>
 /// /////////////////////////////////////////////////////////////////////////////////////////////////
 /// </summary>
-Console.WriteLine("Výsledek: " + strings.Select(x => x.Length).Sum());
+//Console.WriteLine("Výsledek: " + strings.Select(x => x.Length).Sum());
 
 
 // úkol č. 5 ------------------------- 555555555555555555555555555555555
@@ -37,14 +37,17 @@ var listHodnotUpperLower = strings.Select(x => new UpperLowerString(x))
 
 var res = strings.Select(slovo =>(slovo.ToUpper(), slovo.ToLower()));
 
-PrintListGenericky<(string, string)>(res);
+//PrintListGenericky<(string, string)>(res);
 
 /// úkol č. 6 ------------------------- 666666666666666666666666666666666666
 var agg = string.Join("", strings).GroupBy(x => x).Select(g => ( g.Key, Pocet: g.Count(), Znaky: g.Count() )).OrderByDescending(g => g.Pocet);
-PrintListGenericky<(char, int, int)>(agg);
+//PrintListGenericky<(char, int, int)>(agg);
 
     
     Debug.Print("ff");
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 static void PrintList(List<string> coVypsat)
@@ -56,12 +59,24 @@ static void PrintList(List<string> coVypsat)
     }
 }
 
+static Dictionary<char, int>CharFreq (string vstup)
+{
+    var aggDic = string.Join("", vstup).GroupBy(x => x).Select(g => (Key: g.Key, Pocet: g.Count())).OrderByDescending(g => g.Pocet).ThenBy(x => x.Key);
+    Dictionary<char, int> charFreq = new Dictionary<char, int>();
 
+    foreach (var tuple in aggDic)
+    {
+        charFreq.Add(tuple.Key, tuple.Pocet);
+    }
+
+    return charFreq;
+}
 /// <summary>
 /// T - generická metoda
 /// </summary>
 static void PrintListGenericky<T>(IEnumerable<T> coVypsat)
 {
+    
     foreach (var item in coVypsat)
     {
         Console.WriteLine(item);
