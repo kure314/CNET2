662a6bf08ee031921101e4674c4a7542b6307af9
v5416080 <vaclav.kure@saint-gobain.com>
2021-11-24 13:55:18 +0100

    Testování programu - dokončená kapitola

--- CNET2/Tests/UnitTest1.cs
+++ CNET2/Tests/UnitTest1.cs
@@ -1,13 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
 
 namespace Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        static string directory = @"C:\Programovani\bigfiles";
+        // soubory, kde oèekáváme
         [TestMethod]
-        public void TestMethod1()
+        public void FilesExists()
         {
+            
+            var files = Directory.EnumerateFiles(directory, "*.txt");
+            Assert.IsTrue(files != null && files.Count() > 0);
         }
+        [TestMethod]
+        public void LoadBigFile()
+        {
+            var file = Path.Combine(directory, "words01.txt");
+            var lines =File.ReadAllLines(file);
+            Assert.IsTrue(lines.Any());
+            //Assert.
+        }
+
     }
 }
